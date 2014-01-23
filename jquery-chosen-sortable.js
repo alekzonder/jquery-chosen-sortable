bf7b31915e36ffef94efe142fcd80a77687c67ca
alekzonder <alekzonder@gmail.com>
2014-01-23 15:41:05 +0700

    add jquery.valHook

--- jquery-chosen-sortable.js
+++ jquery-chosen-sortable.js
@@ -11,55 +11,70 @@
  */
 (function($) {
 
-  $.fn.chosenOrder = function() {
-    var $this   = this.filter('.chosen-sortable[multiple]').first(),
-        $chosen = $this.siblings('.chosen-container');
+    $.fn.chosenOrder = function() {
+        var $this   = this.filter('.chosen-sortable[multiple]').first(),
+            $chosen = $this.siblings('.chosen-container');
 
-    return $($chosen.find('.chosen-choices li[class!="search-field"]').map( function() {
-      if (!this) {
-        return undefined;
-      }
-      return $this.find('option:contains(' + $(this).text() + ')')[0];
-    }));
-  };
+        return $($chosen.find('.chosen-choices li[class!="search-field"]').map( function() {
+            if (!this) {
+                return undefined;
+            }
+            return $this.find('option:contains(' + $(this).text() + ')')[0];
+        }));
+    };
 
 
-  /*
-   * Extend jQuery
-   */
-  $.fn.chosenSortable = function(){
+    /*
+     * Extend jQuery
+     */
+    $.fn.chosenSortable = function() {
 
-    var $this = this.filter('.chosen-sortable[multiple]');
+        var orignalHook = $.valHooks.select.get;
 
-    $this.each(function(){
-      var $select = $(this);
-      var $chosen = $select.siblings('.chosen-container');
+        $.valHooks.select.get = function(el) {
+            if ($(el).hasClass('chosen-sortable')) {
+                var $select = $(el);
+                var $options = $select.chosenOrder();
+                $select.children().remove();
+                $select.append($options);
+            }
 
-      // On mousedown of choice element,
-      // we don't want to display the dropdown list
-      $chosen.find('.chosen-choices').bind('mousedown', function(event){
-        if ($(event.target).is('span')) {
-          event.stopPropagation();
-        }
-      });
+            return orignalHook(el);
+        };
 
-      // Initialize jQuery UI Sortable
-      $chosen.find('.chosen-choices').sortable({
-        'placeholder' : 'ui-state-highlight',
-        'items'       : 'li:not(.search-field)',
-        //'update'      : _update,
-        'tolerance'   : 'pointer'
-      });
+        var $this = this.filter('.chosen-sortable[multiple]');
 
-      // Intercept form submit & order the chosens
-      $select.closest('form').on('submit', function(){
-        var $options = $select.chosenOrder();
-        $select.children().remove();
-        $select.append($options);
-      });
+        $this.each(function() {
 
-    });
+            var $select = $(this);
 
-  };
+            var $chosen = $select.siblings('.chosen-container');
+
+            // On mousedown of choice element,
+            // we don't want to display the dropdown list
+            $chosen.find('.chosen-choices').bind('mousedown', function(event){
+                if ($(event.target).is('span')) {
+                    event.stopPropagation();
+                }
+            });
+
+            // Initialize jQuery UI Sortable
+            $chosen.find('.chosen-choices').sortable({
+                'placeholder' : 'ui-state-highlight',
+                'items'       : 'li:not(.search-field)',
+                //'update'      : _update,
+                'tolerance'   : 'pointer'
+            });
+
+            // Intercept form submit & order the chosens
+            $select.closest('form').on('submit', function(){
+                var $options = $select.chosenOrder();
+                $select.children().remove();
+                $select.append($options);
+            });
+
+        });
+
+    };
 
 }(jQuery));
